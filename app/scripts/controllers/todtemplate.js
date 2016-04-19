5578b327f3e7b7aff221ed665005e9ae70b99848
Romina Racca <romina.racca@gmail.com>
2016-04-19 16:27:00 -0300

    elimino directiva creada para editor de texto

--- app/scripts/controllers/todtemplate.js
+++ app/scripts/controllers/todtemplate.js
@@ -9,147 +9,102 @@
  */
 angular.module('frontendApp')
 
-  .directive('uncEditor', uncEditor)
+  // .directive('uncEditor', uncEditor)
   .controller('TodtemplateCtrl',  ['$scope', function ($scope) {
 
+    var vm = this;
+    vm.tinymceModel = null;
+
+    vm.myDate = new Date();
+    vm.cityDefault = "Córdoba";
+    vm.recipientDefault = "Sr/a.";
+    vm.refDEfault = "Ref: ";
+
+    vm.tinymceOptions = {
+      selector: 'textarea',           // change this value according to your HTML
+      language_url : 'langs/es.js',   // site absolute URL
+      theme: "modern",                //Hace uso del skin agregado
+      skin: 'light',                  //skin default: lightgray
+
+       menu: {
+         file: {title: 'File', items: 'newdocument | print'},
+         edit: {title: 'Edit', items: 'undo redo | cut copy paste | searchreplace | selectall'},
+         insert: {title: 'Insert', items: 'link | image | hr pagebreak | charmap | template'},
+         view: {title: 'View', items: 'visualaid fullscreen preview'},
+         format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | removeformat'},
+         table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'}
+       },
+       toolbar: 'fontsizeselect bold italic underline strikethrough superscript subscript | removeformat | alignleft aligncenter alignright alignjustify | outdent indent blockquote ltr rtl | bullist numlist | link image hr pagebreak | preview fullscreen',
+
+       plugins: ['hr link image charmap paste print preview anchor pagebreak spellchecker searchreplace visualblocks visualchars',
+       'code fullscreen insertdatetime directionality media nonbreaking save table template textcolor textpattern preview image contextmenu'
+       ],            //Plugins necesarios para que funcionen las herramientas no basicas de toolbar
+
+       //Personaliza el template que se inserta
+       templates: [
+               {title: 'Plantilla Test', description: 'Descripción ...', url:'../../views/templates/templateTest.html'} //put full path to url
+           ],
+
+       // true: Habilita que se utilice el corrector ortografico del browser
+       browser_spellcheck: true,
+
+       // true: deshabilita el menu contextual sobre las palabras en el editor
+       // contextmenu: true,
+
+       //  contextmenu_never_use_native: false,
+
+       //  save_onsavecallback: function () { console.log('Saved'); }
+
+       // Permite escribir en marckdown y se transforma a HTML al dar enter
+       textpattern_patterns: [
+            {start: '*', end: '*', format: 'italic'},
+            {start: '**', end: '**', format: 'bold'},
+            {start: '#', format: 'h1'},
+            {start: '##', format: 'h2'},
+            {start: '###', format: 'h3'},
+            {start: '####', format: 'h4'},
+            {start: '#####', format: 'h5'},
+            {start: '######', format: 'h6'},
+            {start: '1. ', cmd: 'InsertOrderedList'},
+            {start: '* ', cmd: 'InsertUnorderedList'},
+            {start: '- ', cmd: 'InsertUnorderedList'}
+         ],
+
+         // Tamaño del previsualizador
+         // theme_advanced_buttons3_add : "preview",
+         // plugin_preview_width : "400",
+         // plugin_preview_height : "600",
+
+         setup: function(editor) {
+           editor.on('click', function(e) {
+             vm.tinymceModel = 'Time: ' + (new Date());
+             console.log('Editor was clicked');
+           });
+         }
+
+         // Tamaño maximo que el usuario puede estirar toda la interfaz
+         // max_height: newValue
+         //  max_width: 500
+
+         // Alto del area editable en pixeles
+        //  height: newValue;
+   };
+
+   vm.getContent = function() {
+      console.log('Editor content:', vm.tinymceModel);
+    };
+
+    vm.setContent = function() {
+      vm.tinymceModel = 'Time: ' + (new Date());
+    };
 
 
-    // console.log($scope);
-  var vm = this;
-  // vm.tinymceModel = null;
-
-  vm.myDate = new Date();
-  vm.cityDefault = "Córdoba";
-  vm.recipientDefault = "Sr/a.";
-  vm.refDEfault = "Ref: ";
-
-
-
- $scope.getContent = function() {
-    console.log('Editor content:', vm.tinymceModel);
-  };
-
-  $scope.setContent = function() {
-    vm.tinymceModel = 'Time: ' + (new Date());
-  };
-
-
-
- $scope.tinymceOptions = {
-   selector: 'textarea',          // change this value according to your HTML
-   language_url : 'langs/es.js',   // site absolute URL
-   theme: "modern",               //Hace uso del skin agregado
-   skin: 'light',             //skin default: lightgray
-    // menu: {
-    //    view: {title: 'Happy', items: 'code'}
-    // },
-
-
-
-    menu: {
-      file: {title: 'File', items: 'newdocument | print'},
-      edit: {title: 'Edit', items: 'undo redo | cut copy paste | searchreplace | selectall'},
-      insert: {title: 'Insert', items: 'link | image | hr pagebreak | charmap | template'},
-      view: {title: 'View', items: 'visualaid fullscreen preview'},
-      format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | removeformat'},
-      table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'}
-      // tools: {title: 'Tools', items: 'code'}
-    },
-    toolbar: 'fontsizeselect bold italic underline strikethrough superscript subscript | removeformat | alignleft aligncenter alignright alignjustify | outdent indent blockquote ltr rtl | bullist numlist | link image hr pagebreak | preview fullscreen',
-
-    plugins: ['hr link image charmap paste print preview anchor pagebreak spellchecker searchreplace visualblocks visualchars',
-    'code fullscreen insertdatetime directionality media nonbreaking save table template textcolor textpattern preview image contextmenu'
-    ],            //Plugins necesarios para que funcionen las herramientas no basicas de toolbar
-
-    //Personaliza el template que se inserta
-    templates: [
-            {title: 'Plantilla Test', description: 'Descripción ...', url:'../../views/templates/templateTest.html'} //put full path to url
-        ],
-    // template_replace_values : {
-    //                username : "Jack Black",
-    //                staffid : "991234"
-    //        },
-
-    // toolbar:[
-    //   'undo redo | cut copy paste | fontselect fontsizeselect | bold italic underline strikethrough subscript superscript| alignleft aligncenter alignright alignjustify  | outdent indent blockquote | bullist numlist',
-    //   'hr link unlink image charmap pastetext print preview anchor pagebreak spellchecker searchreplace visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-    //   'table | ltr rtl | emoticons template forecolor backcolor'
-    // ],
-
-
-    // true: Habilita que se utilice el corrector ortografico del browser
-    browser_spellcheck: true,
-
-    // true: deshabilita el menu contextual sobre las palabras en el editor
-    // contextmenu: true,
-
-    //  contextmenu_never_use_native: false,
-
-    //  save_onsavecallback: function () { console.log('Saved'); }
-
-    // Permite escribir en marckdown y se transforma a HTML al dar enter
-    textpattern_patterns: [
-         {start: '*', end: '*', format: 'italic'},
-         {start: '**', end: '**', format: 'bold'},
-         {start: '#', format: 'h1'},
-         {start: '##', format: 'h2'},
-         {start: '###', format: 'h3'},
-         {start: '####', format: 'h4'},
-         {start: '#####', format: 'h5'},
-         {start: '######', format: 'h6'},
-         {start: '1. ', cmd: 'InsertOrderedList'},
-         {start: '* ', cmd: 'InsertUnorderedList'},
-         {start: '- ', cmd: 'InsertUnorderedList'}
-      ],
-
-
-      // Tamaño del previsualizador
-      // theme_advanced_buttons3_add : "preview",
-      // plugin_preview_width : "400",
-      // plugin_preview_height : "600",
-
-      // setup: function (ed) {
-      //   var header = "<header>ABCD</header>";
-      //   var footer = "<div>footer</div>";
-      //   var content = ed.getContent();
-      //
-      //   content = header + vm.tinymceModel + footer;
-      //    ed.setContent(content);
-      //
-      // },
-      setup: function(editor) {
-        editor.on('click', function(e) {
-          vm.tinymceModel = 'Time: ' + (new Date());
-          console.log('Editor was clicked');
-        });
-      },
-
-
-   // Tamaño maximo que el usuario puede estirar toda la interfaz
-    max_height: 300,
-   //  max_width: 500
-
-   // Alto del area editable en pixeles
-    height : "300"
-
-  //Para saber si el cuerpo del editor ha sido clickeado
-  //  setup: function(editor) {
-  //   editor.on('click', function(e) {
-  //     console.log('Editor was clicked');
-  //   });
-  // }
-
- };
-
- $scope.getTextBody = function () {
-    console.log(vm.tinymceModel);
-
-
-  //  var und = new upndown();
-  //  und.convert(vm.tinymceModel, function(err, markdown) {
-  //         console.log(markdown);  // Outputs: # Hello, World !
-  //    });
-
+   vm.getTextBody = function () {
+     console.log(vm.tinymceModel);
+    //  var und = new upndown();
+    //  und.convert(vm.tinymceModel, function(err, markdown) {
+    //    console.log(markdown);  // Outputs: # Hello, World !
+    //   });
  };
 
 
@@ -345,103 +300,116 @@ angular.module('frontendApp')
     }
   }]);
 
-  function uncEditor() {
-    var directive = {
-       link: link,
-       template: '<div id="unc-div-editor"><textarea ui-tinymce="tinymceOptions" ng-model="vm.tinymceModel"></textarea></div>',
-       restrict: 'E',
-       scope: {},
-       controller: UncEditorCtrl
-    };
-    return directive;
+  // function uncEditor() {
+  //   var directive = {
+  //      link: link,
+  //      template: '<div id="unc-div-editor"><textarea ui-tinymce="vm.tinymceOptions" ng-model="vm.tinymceModel"></textarea></div>',
+  //      restrict: 'E',
+  //      scope: {},
+  //      controller: UncEditorCtrl,
+  //      controllerAs: 'vm',
+  //      bindToController: true
+  //   };
+  //   return directive;
+  //
+  //   function link(scope, element, attrs) {
+  //     console.log(element);
+  //     scope.altura = element.context.clientHeight;
+  //
+  //  };
 
-    function link(scope, element, attrs) {
-      // console.log(element.context.clientHeight);
-      scope.altura = element.context.clientHeight;
-   };
+    // function UncEditorCtrl($scope) {
+        // var vm = this;
+        // vm.tinymceModel = null;
+        // $scope.altura = null;
+        //   vm.tinymceOptions = null;
 
-    function UncEditorCtrl($scope) {
-        var vm = this;
-        vm.tinymceModel = null;
-        $scope.altura = null;
-
-          $scope.$watch('altura', function (newValue, oldValue) {
-            if (newValue != oldValue) {
-              console.log($scope.altura);
-
-            }
-          });
-
-        vm.tinymceOptions = {
-          selector: 'textarea',          // change this value according to your HTML
-          language_url : 'langs/es.js',   // site absolute URL
-          theme: "modern",               //Hace uso del skin agregado
-          skin: 'light',             //skin default: lightgray
-
-           menu: {
-             file: {title: 'File', items: 'newdocument | print'},
-             edit: {title: 'Edit', items: 'undo redo | cut copy paste | searchreplace | selectall'},
-             insert: {title: 'Insert', items: 'link | image | hr pagebreak | charmap | template'},
-             view: {title: 'View', items: 'visualaid fullscreen preview'},
-             format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | removeformat'},
-             table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'}
-           },
-           toolbar: 'fontsizeselect bold italic underline strikethrough superscript subscript | removeformat | alignleft aligncenter alignright alignjustify | outdent indent blockquote ltr rtl | bullist numlist | link image hr pagebreak | preview fullscreen',
-
-           plugins: ['hr link image charmap paste print preview anchor pagebreak spellchecker searchreplace visualblocks visualchars',
-           'code fullscreen insertdatetime directionality media nonbreaking save table template textcolor textpattern preview image contextmenu'
-           ],            //Plugins necesarios para que funcionen las herramientas no basicas de toolbar
-
-           //Personaliza el template que se inserta
-           templates: [
-                   {title: 'Plantilla Test', description: 'Descripción ...', url:'../../views/templates/templateTest.html'} //put full path to url
-               ],
-
-           // true: Habilita que se utilice el corrector ortografico del browser
-           browser_spellcheck: true,
-
-           // true: deshabilita el menu contextual sobre las palabras en el editor
-           // contextmenu: true,
-
-           //  contextmenu_never_use_native: false,
-
-           //  save_onsavecallback: function () { console.log('Saved'); }
-
-           // Permite escribir en marckdown y se transforma a HTML al dar enter
-           textpattern_patterns: [
-                {start: '*', end: '*', format: 'italic'},
-                {start: '**', end: '**', format: 'bold'},
-                {start: '#', format: 'h1'},
-                {start: '##', format: 'h2'},
-                {start: '###', format: 'h3'},
-                {start: '####', format: 'h4'},
-                {start: '#####', format: 'h5'},
-                {start: '######', format: 'h6'},
-                {start: '1. ', cmd: 'InsertOrderedList'},
-                {start: '* ', cmd: 'InsertUnorderedList'},
-                {start: '- ', cmd: 'InsertUnorderedList'}
-             ],
+          // $scope.$watch('altura', function (newValue, oldValue) {
+        //    // Tamaño maximo que el usuario puede estirar toda la interfaz
+        //       vm.tinymceOptions.max_height = newValue;
+        //    //  max_width: 500
+         //
+          //  $("#unc-div-editor iframe").css("height", 200);
+        //    // Alto del area editable en pixeles
+        //    vm.tinymceOptions.height = newValue;
+          // });
 
 
-             // Tamaño del previsualizador
-             // theme_advanced_buttons3_add : "preview",
-             // plugin_preview_width : "400",
-             // plugin_preview_height : "600",
+                  //   vm.tinymceOptions = {
+                  //     selector: 'textarea',          // change this value according to your HTML
+                  //     language_url : 'langs/es.js',   // site absolute URL
+                  //     theme: "modern",               //Hace uso del skin agregado
+                  //     skin: 'light',             //skin default: lightgray
+                   //
+                  //      menu: {
+                  //        file: {title: 'File', items: 'newdocument | print'},
+                  //        edit: {title: 'Edit', items: 'undo redo | cut copy paste | searchreplace | selectall'},
+                  //        insert: {title: 'Insert', items: 'link | image | hr pagebreak | charmap | template'},
+                  //        view: {title: 'View', items: 'visualaid fullscreen preview'},
+                  //        format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | removeformat'},
+                  //        table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'}
+                  //      },
+                  //      toolbar: 'fontsizeselect bold italic underline strikethrough superscript subscript | removeformat | alignleft aligncenter alignright alignjustify | outdent indent blockquote ltr rtl | bullist numlist | link image hr pagebreak | preview fullscreen',
+                   //
+                  //      plugins: ['hr link image charmap paste print preview anchor pagebreak spellchecker searchreplace visualblocks visualchars',
+                  //      'code fullscreen insertdatetime directionality media nonbreaking save table template textcolor textpattern preview image contextmenu'
+                  //      ],            //Plugins necesarios para que funcionen las herramientas no basicas de toolbar
+                   //
+                  //      //Personaliza el template que se inserta
+                  //      templates: [
+                  //              {title: 'Plantilla Test', description: 'Descripción ...', url:'../../views/templates/templateTest.html'} //put full path to url
+                  //          ],
+                   //
+                  //      // true: Habilita que se utilice el corrector ortografico del browser
+                  //      browser_spellcheck: true,
+                   //
+                  //      // true: deshabilita el menu contextual sobre las palabras en el editor
+                  //      // contextmenu: true,
+                   //
+                  //      //  contextmenu_never_use_native: false,
+                   //
+                  //      //  save_onsavecallback: function () { console.log('Saved'); }
+                   //
+                  //      // Permite escribir en marckdown y se transforma a HTML al dar enter
+                  //      textpattern_patterns: [
+                  //           {start: '*', end: '*', format: 'italic'},
+                  //           {start: '**', end: '**', format: 'bold'},
+                  //           {start: '#', format: 'h1'},
+                  //           {start: '##', format: 'h2'},
+                  //           {start: '###', format: 'h3'},
+                  //           {start: '####', format: 'h4'},
+                  //           {start: '#####', format: 'h5'},
+                  //           {start: '######', format: 'h6'},
+                  //           {start: '1. ', cmd: 'InsertOrderedList'},
+                  //           {start: '* ', cmd: 'InsertUnorderedList'},
+                  //           {start: '- ', cmd: 'InsertUnorderedList'}
+                  //        ],
+                   //
+                   //
+                  //        // Tamaño del previsualizador
+                  //        // theme_advanced_buttons3_add : "preview",
+                  //        // plugin_preview_width : "400",
+                  //        // plugin_preview_height : "600",
+                   //
+                   //
+                  //        setup: function(editor) {
+                  //          editor.on('click', function(e) {
+                  //            vm.tinymceModel = 'Time: ' + (new Date());
+                  //            console.log('Editor was clicked');
+                  //          });
+                  //        }
+                   //
+                   //
+                  //        // Tamaño maximo que el usuario puede estirar toda la interfaz
+                  //        // max_height: newValue
+                  //        //  max_width: 500
+                   //
+                  //        // Alto del area editable en pixeles
+                  //       //  height: newValue;
+                  //  };
 
 
-             setup: function(editor) {
-               editor.on('click', function(e) {
-                 vm.tinymceModel = 'Time: ' + (new Date());
-                 console.log('Editor was clicked');
-               });
-             },
+        //  });
 
-          // Tamaño maximo que el usuario puede estirar toda la interfaz
-          //  max_height: 300,
-          //  max_width: 500
-
-          // Alto del area editable en pixeles
-          //  height : "300"
-        };
-    };
-  };
+    // };
+  // };
